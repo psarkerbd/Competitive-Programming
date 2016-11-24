4e9d9ea4d321e831644d87ec9a0e537a09608bc0
shidhu <meatcse572@gmail.com>
2016-11-24 16:57:00 +0600

    Update Hackerearth City and Flood.cpp

--- Disjoin-Set-union-Find/Hackerearth City and Flood.cpp
+++ Disjoin-Set-union-Find/Hackerearth City and Flood.cpp
@@ -1,6 +1,8 @@
 // Hackerearth City and Flood
 // Accepted
 
+
+// Rough Solution
 #include<bits/stdc++.h>
 
 using namespace std;
@@ -91,3 +93,73 @@ int main()
 
     return 0;
 }
+// End of Rough Solution
+
+
+
+//********************************************************** Standard Solution
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false)
+
+const int high = 100003;
+
+int par[high];
+
+void make_set(int n)
+{
+    for(int i=1; i<=n; i++)
+    {
+        par[i] = i;
+    }
+}
+
+int findd(int n)
+{
+    if(par[n] == n) return n;
+
+    else
+    {
+        return par[n] = findd(par[n]);
+    }
+}
+
+void unionn(int a , int b)
+{
+    int u = findd(a);
+    int v = findd(b);
+    if(u==v) return;
+    else par[u] = v;
+}
+
+int main()
+{
+    fast;
+    int n , i , j , k;
+    cin >> n >> k;
+    make_set(n);
+    while(k--)
+    {
+        cin >> i >> j;
+        unionn(i , j);
+    }
+
+    int cnt = 0;
+
+    for(i=1; i<=n; i++)
+    {
+        if(findd(i) == i)
+        {
+            cnt++;
+        }
+    }
+
+    cout << cnt << "\n";
+
+    return 0;
+}
+
+//************************************* End of Stand Solution
